69b1ab26e1304b02fbb6791cf3743884aa7da270
Dishant Arora <dishantarora16@gmail.com>
2022-02-04 20:42:17 +0530

    initial code

---
+++ code-chef/long-challenges/feb-2022/chefbarber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll m,n;
+        cin>>n>>m;
+        cout<<n*m<<endl;
+    }
+    return 0;
+}
---
+++ gfg/test.cpp
@@ -0,0 +1,9 @@
+// #include<bits/std++.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"hello guys";
+    return 0;
+}
