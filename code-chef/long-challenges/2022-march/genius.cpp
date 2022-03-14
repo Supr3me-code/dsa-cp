3ea527df3322013970c5a1ea42e2995e2214ddc6
Dishant Arora <dishantarora16@gmail.com>
2022-03-15 02:47:24 +0530

    changes

--- code-chef/long-challenges/2022-march/genius.cpp
+++ code-chef/long-challenges/2022-march/genius.cpp
@@ -6,10 +6,20 @@ int main(){
     ll t;cin>>t;
     while(t--){
         ll n,x;cin>>n>>x;
-
+        ll correct, incorrect, unattempted;
         if(x>3*n){
-            //not possible
+            cout<<"NO"<<endl;
+            break;
+        }else{
+            if(x%3==0){
+                correct = x/3;
+                incorrect = 0;
+                unattempted = 0;
+            }else{
+                ll diff = ((x/3)*3) - x;
+                if(diff>)
+            }
         }
-        
+
     }
 }
