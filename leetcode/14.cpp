778456ce53dc9bba0ea40077e9b3592f5ead34a5
Dishant Arora <44555016+Supr3me-code@users.noreply.github.com>
2022-03-23 10:35:57 +0530

    Update 14.cpp

--- leetcode/14.cpp
+++ leetcode/14.cpp
@@ -1,3 +1,23 @@
-#include<iostream>
-using namespace std;
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+        int shortestLength = INT_MAX;
+        for(int i=0;i<strs.size();i++){
+            if(strs[i].size()<shortestLength){
+                shortestLength = strs[i].size();
+            }
+        }
+        
+        for(int i=0;i<shortestLength;i++){
+            char c = strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i] != c){
+                    return prefix;
+                }
+            }
+            prefix += c;
+        }
+        return prefix;
+    }
+};
