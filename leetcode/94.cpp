1d4eddae7313b85c05286be9b564e6c0e600433f
Dishant Arora <dishantarora16@gmail.com>
2022-03-31 02:58:09 +0530

    leetcode

---
+++ leetcode/94.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> inorder;
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        if(root==NULL){
+            return inorder;
+        }
+   
+        inorderTraversal(root->left);
+        inorder.push_back(root->val);
+        inorderTraversal(root->right);
+        
+        
+        return inorder;
+    }
+};
