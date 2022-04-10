3ed672e18e6bc8eb1535a6f2db39cb43214533dc
Dishant Arora <dishantarora16@gmail.com>
2022-04-11 00:17:19 +0530

    day-12

---
+++ code-chef/long-challenges/2022-april-1/xorabc.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+typedef long long int ll;
+using namespace std;
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x;
+        cin>>x;
+
+        if(x%2==0){
+            cout<<0<<" "<<0<<" "<<x/2<<endl;
+        }else{
+            
+        }
+    }
+    return 0;
+}
