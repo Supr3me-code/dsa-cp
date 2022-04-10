85b53686a3c1a113ab88fff111aff6a712edbce6
Dishant Arora <dishantarora16@gmail.com>
2022-04-10 12:42:14 +0530

    day-11

--- code-chef/long-challenges/2022-april-1/notunit.cpp
+++ code-chef/long-challenges/2022-april-1/notunit.cpp
@@ -1,11 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int gcd(int a, int b){
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+bool gcdNotUnit(int x, int y){
+    if(gcd(x,y)>1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 int main(){
     int t;
     cin>>t;
     while(t--){
-        
+        int a,b;
+        bool found = false;
+        cin>>a>>b;
+
+        for(int x=a;x<b;x++){
+            for(int y=x+1;y<=b;y++){
+                if(gcdNotUnit(x,y)){
+                    cout<<x<<" "<<y<<endl;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if(!found)
+            cout<<-1<<endl;
     }
     return 0;
 }
