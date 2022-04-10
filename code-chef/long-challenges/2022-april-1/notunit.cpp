0e3c1a18c94b0ee8efd68ae248f7956a0d755dd7
Dishant Arora <dishantarora16@gmail.com>
2022-04-10 13:41:53 +0530

    day-10

--- code-chef/long-challenges/2022-april-1/notunit.cpp
+++ code-chef/long-challenges/2022-april-1/notunit.cpp
@@ -1,38 +1,28 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int gcd(int a, int b){
-    return b == 0 ? a : gcd(b, a % b);
-}
-
-bool gcdNotUnit(int x, int y){
-    if(gcd(x,y)>1){
-        return true;
-    }else{
-        return false;
-    }
-}
-
 int main(){
-    int t;
-    cin>>t;
+    int t;cin>>t;
     while(t--){
         int a,b;
-        bool found = false;
         cin>>a>>b;
+        int range = b-a+1;
 
-        for(int x=a;x<b;x++){
-            for(int y=x+1;y<=b;y++){
-                if(gcdNotUnit(x,y)){
-                    cout<<x<<" "<<y<<endl;
-                    found = true;
-                    break;
-                }
+        if(a%2==0){
+            if(range>2){
+                cout<<a<<" "<<a+2<<endl;
+            }else{
+                cout<<-1<<endl;
             }
-        }
-
-        if(!found)
+        }else if(range>3){
+            if(a%3==0){
+                cout<<a<<" "<<a+3<<endl;
+            }else{
+                cout<<a+1<<" "<<a+3<<endl;
+            }
+        }else{
             cout<<-1<<endl;
+        }
     }
     return 0;
 }
