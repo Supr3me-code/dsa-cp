f94cfcc52d2eef622af9a73ec480a3a8cee414f0
Dishant Arora <dishantarora16@gmail.com>
2022-05-06 20:53:13 +0530

    fcf

---
+++ code-chef/long-challenges/2022-may-1/sugarcane.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;cin>>n;
+        int total = 50*n;
+        int profit = 0.30*total;
+        cout<<profit<<endl;
+    }
+    return 0;
+}
