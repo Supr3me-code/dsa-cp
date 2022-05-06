cf5f4ce92a9c15a02415ee1fba562349f3b56093
Dishant Arora <dishantarora16@gmail.com>
2022-05-06 20:42:45 +0530

    hmm

---
+++ code-chef/long-challenges/2022-may-1/footcup.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--) {
+        int x,y;
+        cin>>x>>y;
+        if(x==y && x!=0 && y!=0){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
