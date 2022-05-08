9dc04ee29730244b15699d83e2ed81de528b674b
Dishant Arora <dishantarora16@gmail.com>
2022-05-08 23:14:36 +0530

    ads

--- code-chef/long-challenges/2022-may-1/push7pa.cpp
+++ code-chef/long-challenges/2022-may-1/push7pa.cpp
@@ -6,7 +6,19 @@ int main(){
     int t;
     cin>>t;
     while(t--) {
-        
+        int n;
+        cin>>n;
+        int arr[n], m=0;
+        map<int, int> map1;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            m = max(m, arr[i]);
+            map1[arr[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            m = max(m, arr[i]+map1[arr[i]]-1);
+        }
+        cout<<m<<endl;
     }
     return 0;
 }
