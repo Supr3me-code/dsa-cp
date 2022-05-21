d52fe5f4168d22ec8a9eb2dd1662bce8809c6d42
Dishant Arora <dishantarora16@gmail.com>
2022-05-21 14:39:03 +0530

    nice

--- code-chef/long-challenges/2022-may-2/xor_pal.cpp
+++ code-chef/long-challenges/2022-may-2/xor_pal.cpp
@@ -20,8 +20,12 @@ int main(){
              start++;
              end--;
          }
+         int res = count/2 + count%2; //if there are 2 pairs that mismatch, they can be corrected in 1 step
+         //ex. 1111000 -> 1 step: xor bin[0] and bin[1]; (both 1s become 0 and matches with the corresponding 0s on the right side of the string)
+         //then xor bin[2] and bin[4]; 
+         // so if there are n pairs that mismatch -> can be solved in n/2 steps.. if n is odd, 1 pair will be left so xor the 0 and 1 to make them 1
 
-         cout<<count<<endl;
+         cout<<res<<endl;
     }
     return 0;
 }
