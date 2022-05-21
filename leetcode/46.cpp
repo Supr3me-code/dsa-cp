33acd54cc9e10831b15089db77fd48d9f2ef99cb
Dishant Arora <dishantarora16@gmail.com>
2022-05-21 17:00:25 +0530

    21 may

--- leetcode/46.cpp
+++ leetcode/46.cpp
@@ -1,5 +1,3 @@
-// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
-
 class Solution {
 public:
     void recurCall(vector<int> &nums, vector<vector<int>> &answer, int idx){
