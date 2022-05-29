d42009bcee9170ea6ae2cc4d70c201591e1f47c2
Dishant Arora <dishantarora16@gmail.com>
2022-05-30 01:29:32 +0530

    30 may

--- striver-dsa-sheet/binary-search/find-element-that-appear-once-rest-appear-twice.cpp
+++ striver-dsa-sheet/binary-search/find-element-that-appear-once-rest-appear-twice.cpp
@@ -14,11 +14,22 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         
         int left = 0;
-        int right = nums.size()-2;
+        int right = nums.size()-2; // marked the second last element as the right and not the last element as if the answer is at the last index
+        // we want left and right to cross each other but in this case left will never be able to cross right as right will keep pointing at the
+        // last element and there's nothing to the right of it to cross over to
         
+
+        //observation: 
+            // 1. to the left of the answer each 1st number of a pair has an even index -> 2nd number has odd index
+            // 2. to the right of the answer each 1st number of a pair has an odd index -> --"--          even index
+
         while(left<=right){
             int mid = (left+right)/2;
-            if(nums[mid] == nums[mid^1])
+            // if mid==odd then mid^1=="even number just before it"
+            // if mid==even then mid^1=="odd number just after it"
+            // so if the numbers at these 2 indices are equal then according to the observation, 
+            //they belong to the left of the answer so we should check to the right of nums[mid] , therefore left = mid+1
+            if(nums[mid] == nums[mid^1]) 
                 left = mid+1;
             else
                 right = mid-1;
