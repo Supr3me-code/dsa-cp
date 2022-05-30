3741485e344285ca69fb4c5c39f4be193aa238e9
Dishant Arora <dishantarora16@gmail.com>
2022-05-30 23:28:46 +0530

    30 may

--- gfg/medium/kth-element-of-2-sorted-arrays.cpp
+++ gfg/medium/kth-element-of-2-sorted-arrays.cpp
@@ -1,7 +1,3 @@
-/*
-Given two sorted arrays arr1 and arr2 of size N and M respectively and an element K. The task is to find the element that would be at the k’th position of the final sorted array.
-*/
-
 class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k){
