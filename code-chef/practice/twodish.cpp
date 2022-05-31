d83c8e836a7aa73c8b828675a343e91ab76f47f3
Dishant Arora <dishantarora16@gmail.com>
2022-05-31 08:30:06 +0530

    31 may

--- code-chef/practice/twodish.cpp
+++ code-chef/practice/twodish.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main(){
     int t;cin>>t;
     while(t--){
-        
+        int n,a,b,c;
+        cin>>n>>a>>b>>c;
+        if((a+c)>= n && b>=n){
+            cout<<"Yes"<<endl;
+        }else{
+            cout<<"No"<<endl;
+        }
     }
     return 0;
 }
