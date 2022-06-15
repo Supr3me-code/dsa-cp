cbda31aa60d5c096862af62d5fc6d11b2dc4048d
Dishant Arora <dishantarora16@gmail.com>
2022-06-16 02:24:49 +0530

    16 june

--- code-chef/lunch-time/2022-april/ltime.cpp
+++ code-chef/lunch-time/2022-april/ltime.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int x;cin>>x;
-        if(x>=1 && x<=4){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        if (x >= 1 && x <= 4) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
         }
     }
     return 0;
