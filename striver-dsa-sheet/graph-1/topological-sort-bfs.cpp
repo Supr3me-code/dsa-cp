46dd587f90e9ee6f83caf4d79bc0651f3d3bd560
Dishant Arora <dishantarora16@gmail.com>
2022-07-01 14:44:20 +0530

    1 july

---
+++ striver-dsa-sheet/graph-1/topological-sort-bfs.cpp
@@ -0,0 +1,34 @@
+// Kahn's Algorithm
+
+class Solution {
+   public:
+    vector<int> topoSort(int N, vector<int> adj[]) {
+        queue<int> q;
+        vector<int> indegree(N, 0);
+        for (int i = 0; i < N; i++) {
+            for (auto it : adj[i]) {
+                indegree[i]++;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegee[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return topo;
+    }
+}
