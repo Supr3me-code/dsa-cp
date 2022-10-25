a1c97f8f5017ee855b66d0e61e233a5d060f2d87
Dishant Arora <dishantarora16@gmail.com>
2022-10-25 15:03:05 +0530

    25 oct

---
+++ code-chef/LP2TO301/basic-programming/indiperm.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 1; i < n; i++) {
+            cout << i + 1 << " ";
+        }
+        cout << 1 << endl;
+    }
+    // your code goes here
+    return 0;
+}
