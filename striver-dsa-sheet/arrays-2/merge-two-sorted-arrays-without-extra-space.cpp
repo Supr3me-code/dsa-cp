29f50e930471e2377c48a39dea214a5ab013581e
Dishant Arora <dishantarora16@gmail.com>
2023-03-11 16:15:52 +0530

    codes added

--- striver-dsa-sheet/arrays-2/merge-two-sorted-arrays-without-extra-space.cpp
+++ striver-dsa-sheet/arrays-2/merge-two-sorted-arrays-without-extra-space.cpp
@@ -1,19 +1,42 @@
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing
+// order, and two integers m and n, representing the number of elements in nums1
+// and nums2 respectively.
 
 // Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
-// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-
-
+// The final sorted array should not be returned by the function, but instead be
+// stored inside the array nums1. To accommodate this, nums1 has a length of m +
+// n, where the first m elements denote the elements that should be merged, and
+// the last n elements are set to 0 and should be ignored. nums2 has a length of
+// n.
 
 class Solution {
-public:
+   public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        for(int i=m;i<n+m;i++){
-            nums1[i] = nums2[i-m];
+        for (int i = m; i < n + m; i++) {
+            nums1[i] = nums2[i - m];
         }
-        
+
         sort(nums1.begin(), nums1.end());
     }
 };
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m,
+                                 int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int lastIndex = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && arr1[i] > arr2[j]) {
+            arr1[lastIndex] = arr1[i];
+            i--;
+        } else {
+            arr1[lastIndex] = arr2[j];
+            j--;
+        }
+
+        lastIndex--;
+    }
+    return arr1;
+}
--- striver-dsa-sheet/arrays-2/repeat-and-missing-number.cpp
+++ striver-dsa-sheet/arrays-2/repeat-and-missing-number.cpp
@@ -1,6 +1,29 @@
-// There are certain problems which are asked in the interview to also check how you take care of overflows in your problem.
+// There are certain problems which are asked in the interview to also check how
+// you take care of overflows in your problem.
 
 // This is one of those problems.
 
-// Please take extra care to make sure that you are type-casting your ints to long properly and at all places. Try to verify if your solution works if number of elements is as large as 105
+// Please take extra care to make sure that you are type-casting your ints to
+// long properly and at all places. Try to verify if your solution works if
+// number of elements is as large as 105
 
+vector<int> missing_repeated_number(const vector<int> &A) {
+    long long int len = A.size();
+    long long int S = (len * (len + 1)) / 2;
+    long long int P = (len * (len + 1) * (2 * len + 1)) / 6;
+    long long int missingNumber = 0, repeating = 0;
+
+    for (int i = 0; i < A.size(); i++) {
+        S -= (long long int)A[i];
+        P -= (long long int)A[i] * (long long int)A[i];
+    }
+
+    missingNumber = (S + P / S) / 2;
+    repeating = missingNumber - S;
+    vector<int> ans;
+
+    ans.push_back(repeating);
+    ans.push_back(missingNumber);
+
+    return ans;
+}
